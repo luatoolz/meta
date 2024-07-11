ca93a8b706744ca338a51d44e3f34ba7cc44bd57
luatoolz <luatoolz@somahorse.com>
2024-07-12 00:27:31 +0300

    fix meta.require

--- lua/meta/require.lua
+++ lua/meta/require.lua
@@ -1,8 +1,7 @@
 local no = require "meta.no"
 
 return function(p, ...)
-  return function(m, toerror)
-    toerror=type(toerror)=='nil' and true or toerror
+  return function(m)
     assert(type(m)=='string', 'require want string, got ' .. type(m))
     if m:startswith('.') then m=no.sub(no.parent(p), m:lstrip('.')) end
     return no.require(m, true)
