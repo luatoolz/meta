7765d7e0d9d8eadd10b9ad6f2577cc62ac67ce7c
luatoolz <luatoolz@somahorse.com>
2024-09-27 11:29:58 +0300

    fix: more robust tonumber() in meta.math

--- lua/meta/math.lua
+++ lua/meta/math.lua
@@ -1,5 +1,7 @@
 require "compat53"
 
+local getmetatable = debug and debug.getmetatable or getmetatable
+
 local math_floor = math.floor
 
 if not math.round then
@@ -11,9 +13,18 @@ end
 assert(tonumber)
 local _tonumber = tonumber
 tonumber = function(x, base)
-  if type(x)=='table' then
-    local tn = (getmetatable(x) or {}).__tonumber
+  if type(x)=='number' then return x end
+  if type(x)=='table' and not getmetatable(x) then
+    local it = ipairs(x)
+    it=it(x,0)
+    if type(it)=='number' then return #x end
+    return
+  end
+  if (type(x)=='table' or type(x)=='userdata') and getmetatable(x) then
+    local mt=getmetatable(x)
+    local tn=mt.__tonumber or mt.__len
     if type(tn)=='function' then return tn(x, base) end
+    return
   end
   return _tonumber(x, base)
 end
