dbcbb8d419ebd47850a7a26ebb1f5d0de5b621f9
luatoolz <luatoolz@somahorse.com>
2024-10-02 20:16:43 +0300

    fix: toboolean

--- lua/meta/boolean.lua
+++ lua/meta/boolean.lua
@@ -1,28 +1,27 @@
 require "compat53"
 require 'meta.string'
 local getmetatable = debug and debug.getmetatable or getmetatable
-local booleans={
+toboolean=setmetatable({
   [0]=false,
   ["0"]=false,
   ["false"]=false,
   [""]=false,
   [false]=false,
   ['nil']=false,
-}
-
-toboolean={}
-return setmetatable(toboolean, {
+}, {
 __call=function(self, it) return self[it] end,
 __index=function(self, it)
+  if type(it)=='boolean' then return it end
   if type(it)=='table' or type(it)=='userdata' then
     local tb=(getmetatable(it) or {}).__toboolean
     if type(tb)=='function' then return tb(it) end
-    return type(next(it))~='nil'
   end
+  if type(it)=='table' then return type(next(it))~='nil' end
   return (type(it)~='nil'
-    and rawget(booleans, tostring(it):lower())~=false
+    and rawget(self, tostring(it):lower())~=false
     and (type(it)~='string' or not it:match("^%s+$"))
     and it)
   and true or false
 end,
 })
+return toboolean
