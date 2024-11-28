36abd554ef1b00b538f17a007f548cea0db86ddc
luatoolz <luatoolz@somahorse.com>
2024-11-28 15:19:27 +0300

    +table.sub +spec

--- lua/meta/table.lua
+++ lua/meta/table.lua
@@ -32,7 +32,7 @@ function maxi(self) return is.table(self) and (maxn and maxn(self) or #self) end
 table.maxi=maxi
 
 -- __preserve=true: try to preserve argument type (specific array/set/hash/list should use it), not for loader/modules/cache/etc
-function preserve(self)
+local function preserve(self)
 --  print('PRESERVE', getmetatable(self).__name, (is.table(self) and getmetatable(self) and mt(self).__preserve))
   return (is.table(self) and getmetatable(self) and mt(self).__preserve) and self() or table() end
 
@@ -214,6 +214,27 @@ function table.range(...)
     if i>to then return end
     return i
   end
+end
+
+-- string.sub for table
+-- todo: boundary control
+function table:sub(i,j)
+  local a,b=i,j
+  if type(self)~='table' then return nil end
+  local rv={} --preserve(self)
+  if #self==0 then return rv end
+  i=i or 1
+  j=j or #self
+  if type(i)~='number' or type(j)~='number' then return rv end
+  if i<0 then i=(#self+1)+i end; if i<1 then i=1 end
+  if j<0 then j=(#self+1)+j end; if j<1 then j=1 end
+  if i>#self then i=#self end
+  if j>#self then j=#self end
+  while i<=j do
+    table.insert(rv, self[i])
+    i=i+1
+  end
+  return rv
 end
 
 -- t:values(true)  -- only non-numeric keys
---
+++ spec/table/sub_spec.lua
@@ -0,0 +1,57 @@
+describe("table.sub", function()
+  local meta, is, sub
+  setup(function()
+    meta = require "meta"
+    is = meta.is
+    sub = table.sub
+  end)
+  it("meta", function()
+    assert.truthy(is)
+    assert.truthy(is.callable(sub))
+  end)
+  it("positive", function()
+    local z={1,2,3,4,5}
+
+    assert.same({1}, sub(z,1,1))
+    assert.same({},sub({},1,5))
+
+    assert.same({1}, sub(z,1,1))
+    assert.same({1,2}, sub(z,1,2))
+    assert.same({1,2,3}, sub(z,1,3))
+    assert.same({1,2,3,4}, sub(z,1,4))
+    assert.same(z, sub(z,1,5))
+
+    assert.same(z, sub(z,1,5))
+    assert.same({2,3,4,5}, sub(z,2,5))
+    assert.same({3,4,5}, sub(z,3,5))
+    assert.same({4,5}, sub(z,4,5))
+    assert.same({5}, sub(z,5,5))
+
+    assert.same({1}, sub(z,1,-5))
+    assert.same({1,2}, sub(z,1,-4))
+    assert.same({1,2,3}, sub(z,1,-3))
+    assert.same({1,2,3,4}, sub(z,1,-2))
+    assert.same(z, sub(z,1,-1))
+
+    assert.same(z, sub(z,-5,5))
+    assert.same({2,3,4,5}, sub(z,-4,5))
+    assert.same({3,4,5}, sub(z,-3,5))
+    assert.same({4,5}, sub(z,-2,5))
+    assert.same({5}, sub(z,-1,5))
+
+    assert.same(z, sub(z,1))
+    assert.same(z, sub(z))
+    assert.same({5}, sub(z,-1))
+  end)
+  it("negative", function()
+    assert.is_nil(sub(''))
+    assert.is_nil(sub(0))
+    assert.is_nil(sub(1))
+    assert.is_nil(sub(false))
+    assert.is_nil(sub(true))
+  end)
+  it("nil", function()
+    assert.is_nil(sub())
+    assert.is_nil(sub(nil))
+  end)
+end)
