545f97cc4726a02f3383e2160fdfd27cd6d60d08
luatoolz <luatoolz@somahorse.com>
2025-01-14 20:12:55 +0300

    fix meta.select +spec

--- lua/meta/select.lua
+++ lua/meta/select.lua
@@ -1,13 +1,12 @@
 require "meta.string"
 return setmetatable({},{
 __call=function(self, ...)
-  local len=select('#',...)
-  local __args={...}
-  if len==0 or type(__args[1])=='nil' then return nil end
+  if select('#',...)==0 then return nil end
+  local args={}
+  for i,v in ipairs({...}) do if type(v)~='nil' then table.insert(args, v) end end
+  if #args==0 then return nil end
   return function(x)
     if type(x)~='table' then return nil end
-    local args=__args
-    if #args==1 and type(args[1])=='table' then args=args[1] end
     local rv={}
     if #args>0 then
       for i,k in ipairs(args) do rv[k]=x[k] end
@@ -19,23 +18,9 @@ __call=function(self, ...)
 end,
 __index=function(self, key)
   local k=key
-  return function(x, b)
+  return function(x)
     if type(x)=='table' then
-      if type(x[k])~='nil' then return x[k], k end
-      if #x>0 then
-        local rv={}
-        for i,v in ipairs(x) do
-          if type(v)=='table' and type(v[k])~='nil' then table.insert(rv,v[k]) end
-        end
-        if #rv>0 then return rv end
-      elseif type(next(x))~='nil' then
-        local rv={}
-        for kk,v in pairs(x) do
-          if type(v)=='table' and type(v[k])~='nil' then rv[kk]=v[k] end
-        end
-        if type(next(rv))~='nil' then return rv end
-      end
-      if x==k then if type(b)=='table' then return b[k], k end end
+      return x[k]
     end
   end
 end,
