285fe39434e909c735c710fed49906ef3decea75
luatoolz <luatoolz@somahorse.com>
2025-01-25 04:20:45 +0300

    add argument to computed/computable

--- lua/meta/mt/computable.lua
+++ lua/meta/mt/computable.lua
@@ -1,5 +1,5 @@
 local xpcall = require "meta.xpcall"
-return function(self, t, key)
+return function(self, t, key, ...)
   if type(t)=='nil' or (type(t)=='table' and not next(t)) or type(key)=='nil' then return nil end
-  return xpcall(rawget(t, key), nil, self)
+  return xpcall(rawget(t, key), nil, self, ...)
   end
