665a4d954d1f3c75d735e8a69b577b5b562aa091
luatoolz <luatoolz@somahorse.com>
2025-01-25 04:19:33 +0300

    +math.round10

--- lua/meta/math.lua
+++ lua/meta/math.lua
@@ -7,4 +7,15 @@ if not math.round then
   end
 end
 
+if not math.round10 then
+  function math.round10(it, decimals)
+    if type(it)~='string' then return it end
+    if (not decimals) then return math.round(it) end
+    local multiplier = 10 ^ decimals
+    local left = it%1
+    local int = it-left
+    return int + math.round(left * multiplier) / multiplier
+  end
+end
+
 math.randomseed(os.time())
